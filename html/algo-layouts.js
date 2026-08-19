3a48714f558f1462ca68d8e5e46495b31d2aea3f
Alexey S. Kompaniets <kompadre@gmail.com>
2026-08-19 18:58:51 +0200

    Center algorithm 3 columns

--- html/algo-layouts.js
+++ html/algo-layouts.js
@@ -1,7 +1,7 @@
 export const ALGO_LAYOUTS = [
     [[null,null,"Op1","Op3",null,null],[null,null,"Op2","Op4",null,null],[null,null,null,"Op5",null,null],[null,null,null,"Op6",null,null]],
     [[null,null,"Op1","Op3",null,null],[null,null,"Op2","Op4",null,null],[null,null,null,"Op5",null,null],[null,null,null,"Op6",null,null]],
-    [["Op1",null,"Op4",null,null,null],["Op2",null,"Op5",null,null,null],["Op3",null,"Op6",null,null,null],[null,null,null,null,null,null]],
+    [[null,null,"Op1",null,"Op4",null],[null,null,"Op2",null,"Op5",null],[null,null,"Op3",null,"Op6",null],[null,null,null,null,null,null]],
     [["Op1",null,null,"Op2",null,"Op3"],[null,null,"Op4",null,null,"Op5"],[null,null,null,null,null,"Op6"],[null,null,null,null,null,null]],
     [[null,null,"Op1","Op2",null,"Op3"],[null,null,"Op5","Op3",null,"Op4"],[null,null,null,null,null,"Op6"],[null,null,null,null,null,null]],
     [[null,null,"Op1","Op2","Op3","Op4"],[null,null,"Op5","Op3",null,"Op4"],[null,null,null,null,null,"Op6"],[null,null,null,null,null,null]],
