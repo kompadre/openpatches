ca7391a29b400a104b605218f3b2f49305a1f6cb
Alexey S. Kompaniets <kompadre@gmail.com>
2026-08-19 22:33:57 +0200

    Shift algo 28 ops 3-6 one cell left

--- html/algo-layouts.js
+++ html/algo-layouts.js
@@ -26,7 +26,7 @@ export const ALGO_LAYOUTS = [
     [[null,"Op1","Op2","Op3","Op4","Op5"],[null,null,null,"Op6",null,null],[null,null,null,null,null,null],[null,null,null,null,null,null]],
     [[null,"Op1","Op2",null,"Op4",null],[null,null,"Op3","Op5","Op6",null],[null,null,null,null,null,null],[null,null,null,null,null,null]],
     [[null,"Op1","Op2",null,"Op4",null],[null,null,"Op3","Op5","Op6",null],[null,null,null,null,null,null],[null,null,null,null,null,null]],
-    [[null,null,"Op1",null,"Op3","Op6"],[null,null,"Op2",null,"Op4",null],[null,null,null,null,"Op5",null],[null,null,null,null,null,null]],
+    [[null,null,"Op1","Op3","Op6",null],[null,null,"Op2","Op4",null,null],[null,null,null,"Op5",null,null],[null,null,null,null,null,null]],
     [[null,"Op1","Op2","Op3","Op5",null],[null,null,null,"Op4","Op6",null],[null,null,null,null,null,null],[null,null,null,null,null,null]],
     [[null,"Op1","Op2","Op3","Op6",null],[null,null,null,"Op4",null,null],[null,null,null,"Op5",null,null],[null,null,null,null,null,null]],
     [["Op1","Op2","Op3","Op4","Op5",null],[null,null,null,null,"Op6",null],[null,null,null,null,null,null],[null,null,null,null,null,null]],
