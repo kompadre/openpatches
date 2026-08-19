db5fef080ee4545cd01a3c1e9a56b11bde0b1d1b
Alexey S. Kompaniets <kompadre@gmail.com>
2026-08-19 22:43:39 +0200

    Shift algorithm 11 one more cell left

--- html/algo-layouts.js
+++ html/algo-layouts.js
@@ -9,7 +9,7 @@ export const ALGO_LAYOUTS = [
     [[null,"Op1","Op3",null,null,null],[null,"Op2","Op4","Op5",null,null],[null,null,null,"Op6",null,null],[null,null,null,null,null,null]],
     [[null,"Op1","Op3",null,null,null],[null,"Op2","Op4","Op5",null,null],[null,null,null,"Op6",null,null],[null,null,null,null,null,null]],
     [[null,"Op4","Op1",null,null,null],["Op5","Op6","Op2",null,null,null],[null,null,"Op3",null,null,null],[null,null,null,null,null,null]],
-    [[null,null,null,"Op4","Op1",null],[null,null,"Op5","Op6","Op2",null],[null,null,null,null,"Op3",null],[null,null,null,null,null,null]],
+    [[null,null,"Op4","Op1",null,null],[null,"Op5","Op6","Op2",null,null],[null,null,null,"Op3",null,null],[null,null,null,null,null,null]],
     [[null,null,"Op3",null,"Op1",null],[null,"Op4","Op5","Op6","Op2",null],[null,null,null,null,null,null],[null,null,null,null,null,null]],
     [[null,"Op3",null,"Op1",null,null],["Op4","Op5","Op6","Op2",null,null],[null,null,null,null,null,null],[null,null,null,null,null,null]],
     [[null,null,"Op1","Op3",null,null],[null,null,"Op2","Op4",null,null],[null,null,"Op6","Op5",null,null],[null,null,null,null,null,null]],
